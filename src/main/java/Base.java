4648be771ca863974e68b71681054b0722550080
Zubritsky_Dmitriy <dszubritsky@gmail.com>
2018-03-04 20:28:38 +0300

    Реализоват класс Base (точка входа в пограмму). 3 пункт в задание №3

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -2,9 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * Create by Zubritskiy Dmitriy on 04.03.2018
+ *@author Zubritskiy Dmitriy
+ *
+ */
 public class Base {
     public static void main(String[] args) throws IOException{
-        System.out.println("Hello, World");
+        System.out.println("Выберите операцию: (1- Калькулятор, 2 - Поиск максимального числа в массиве)");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(rd.readLine());
 
+        if (num == 1){
+          new Calculator().CalculatorMain();
+        }
+        else if (num == 2){
+            SearchMaxMasiv.main(args);
+        }
     }
 }
