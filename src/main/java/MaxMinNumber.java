c9f6ae8a6b007365e44aa34e16d2a560286c1e1c
Zubritsky_Dmitriy <dszubritsky@gmail.com>
2018-03-10 20:19:09 +0300

    Задание №4 пункт 1

--- src/main/java/MaxMinNumber.java
+++ src/main/java/MaxMinNumber.java
@@ -1,8 +1,16 @@
+/**
+ * Create by Zubritskiy Dmitriy on 10.03.2018
+ * Задание №4
+ * Подсчет минимального положительного числа включает 0(как положительное число)
+ *@author Zubritskiy Dmitriy
+ *
+ */
+
 public class MaxMinNumber {
     public static void main(String[] args) {
         int[] array = new int[20];  //Инициализация масива размерностью 20
         int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
         int nMax = 0;
         int nMin = 0;
 
@@ -13,18 +21,21 @@ public class MaxMinNumber {
                 nMin = i;
 
             }
-            else if (array[i] > max) {
-                max = array[i];
-                nMax = i;
+            else if (array[i] < max) {
+                if (array[i] >= 0 ){
+                    max = array[i];
+                    nMax = i;
+                }
+
             }
             System.out.print(array[i] + " ");
 
             }
         System.out.println();
-        System.out.println("Minimum is " + min);
-        System.out.println("Maximum is " + max);
-        System.out.println("Номер Минимального элемента в массиве " + nMin);
-        System.out.println("Номер Максимального элеманта в массиве " + nMax);
+        System.out.println("Максимальное отрицательное число: " + min);
+        System.out.println("Минимальное положительное число: " + max);
+        System.out.println("Номер максимального отрицательного элемента в массиве " + nMin);
+        System.out.println("Номер минимального положительного элеманта в массиве " + nMax);
         array[nMin] = max;
         array[nMax] = min;
         for (int i = 0; i < 20; i++){
